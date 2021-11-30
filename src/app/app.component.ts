42afc455132e2b7ff4f2450b1bde6e1d221b3ef7
gmagyar <gmagyar@freelancer.com>
2021-11-30 19:21:15 +0000

    minimal reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {locationPresets} from "./mock/location.mock";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-side-effect';
+  title = locationPresets['Sydney'];
 }
---
+++ src/app/mock/location.mock.ts
@@ -0,0 +1,11 @@
+export const locationPresets = {
+  'Sydney': {
+    countryCode: 'AU',
+    city: 'Sydney',
+    mapCoordinates: {
+      latitude: 41.876,
+      longitude: -87.64,
+    },
+    vicinity: 'Sydney',
+  },
+};
